d13e922faba1aecac76dca7b7dfafee5f96df81e
Shoiya Akari <akariondev@gmail.com>
2024-10-27 16:48:00 -0300

    bot.sh: remove flag --variant

--- bot.sh
+++ bot.sh
@@ -43,7 +43,7 @@ start_message=$(curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMess
 start_time=$(date +%s)
 
 ./ksu_update.sh
-./moe.sh --variant fts
+./moe.sh
 
 if [[ $? -eq 0 ]]; then
     commit_head=$(git log --oneline -1 --pretty=format:'%h - %an')
