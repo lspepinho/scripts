af91c16ffa69c9d31bcb49a4ada97c35147f9a60
Shoiya Akari <akariondev@gmail.com>
2024-10-28 01:21:13 -0300

    bot.sh: perf info in CI

--- bot.sh
+++ bot.sh
@@ -82,7 +82,7 @@ EOF
     build_info=$(cat <<EOF
 *bangkk build (#${build_count}) has succeeded*
 *Kernel Version*: ${kernel_version}
-*Build Type*: \`${build_type}\` *(KSU/Fourteen)*
+*Build Type*: \`${build_type}\` *(KSU+Perf/Fourteen)*
 *Tag*: \`${tag}\`
 
 *Duration*: ${elapsed_minutes} Minutes ${elapsed_seconds} Seconds
