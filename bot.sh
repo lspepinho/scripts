6e47209e66b422501dd46971dfbd2fea4d0b4072
Shoiya Akari <akariondev@gmail.com>
2024-12-25 12:38:05 -0300

    bot.sh: update tag version to fifteen.

--- bot.sh
+++ bot.sh
@@ -82,7 +82,7 @@ EOF
     build_info=$(cat <<EOF
 *ginkgo build (#${build_count}) has succeeded*
 *Kernel Version*: ${kernel_version}
-*Build Type*: \`${build_type}\` *(KSU+EroFS/Fourteen)*
+*Build Type*: \`${build_type}\` *(KSU+EroFS/Fifteen)*
 *Tag*: \`${tag}\`
 
 *Duration*: ${elapsed_minutes} Minutes ${elapsed_seconds} Seconds
