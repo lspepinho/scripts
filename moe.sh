0ccc96246e44fd4652da6019eadb6b80df4e2018
Shoiya Akari <akariondev@gmail.com>
2025-01-14 22:13:12 -0300

    moe.sh: don't use dtbo.img

--- moe.sh
+++ moe.sh
@@ -75,7 +75,7 @@ make -j$(nproc --all) \
     CROSS_COMPILE=$GCC_64_DIR/bin/aarch64-linux-android- \
     CROSS_COMPILE_ARM32=$GCC_32_DIR/bin/arm-linux-androideabi- \
     CLANG_TRIPLE=aarch64-linux-gnu- \
-    Image.gz-dtb dtbo.img
+    Image.gz-dtb
 
 if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && \
    [ -f "out/arch/arm64/boot/dtbo.img" ]; then
