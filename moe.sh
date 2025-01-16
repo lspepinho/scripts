1ee555f6c05622fdc941f655423217bcfcd398c5
Shoiya Akari <akariondev@gmail.com>
2025-01-15 23:10:51 -0300

    Update moe.sh

--- moe.sh
+++ moe.sh
@@ -8,7 +8,7 @@ TC_DIR="$HOME/tc/clang-20.0.0"
 GCC_64_DIR="$HOME/tc/aarch64-linux-android-15.0"
 GCC_32_DIR="$HOME/tc/arm-linux-androideabi-15.0"
 AK3_DIR="$HOME/android/AnyKernel3"
-DEFCONFIG="vendor/moe_defconfig"
+DEFCONFIG=‎"java_defconfig"
 
 export PATH="$TC_DIR/bin:$PATH"
 
@@ -75,26 +75,24 @@ make -j$(nproc --all) \
     CROSS_COMPILE=$GCC_64_DIR/bin/aarch64-linux-android- \
     CROSS_COMPILE_ARM32=$GCC_32_DIR/bin/arm-linux-androideabi- \
     CLANG_TRIPLE=aarch64-linux-gnu- \
-    Image.gz-dtb dtbo.img
+    Image.gz
 
-if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && \
-   [ -f "out/arch/arm64/boot/dtbo.img" ]; then
+if [ -f "out/arch/arm64/boot/Image.gz" ] then
     echo -e "\nKernel compiled successfully! Zipping up...\n"
     if [ -d "$AK3_DIR" ]; then
         cp -r $AK3_DIR AnyKernel3
-    elif ! git clone -q https://github.com/MoeKernel/AnyKernel3; then
+    elif ! git clone -q -b java https://github.com/MoeKernel/AnyKernel3; then
         echo -e "\nAnyKernel3 repo not found locally and cloning failed! Aborting..."
         exit 1
     fi
-    cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
-    cp out/arch/arm64/boot/dtbo.img AnyKernel3
+    cp out/arch/arm64/boot/Image.gz AnyKernel3
     rm -f *zip
     cd AnyKernel3
-    git checkout master &> /dev/null
+    git checkout java &> /dev/null
     zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
     cd ..
     rm -rf AnyKernel3
-    rm -rf out/arch/arm64/boot
+    #rm -rf out/arch/arm64/boot
     echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
     echo "Zip: $ZIPNAME"
 else
