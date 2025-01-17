add6c24056f09ceee65530be93001a7d53bbd47b
Shoiya Akari <akariondev@gmail.com>
2025-01-17 11:18:37 -0300

    Update moe.sh

--- moe.sh
+++ moe.sh
@@ -4,7 +4,7 @@
 
 SECONDS=0
 ZIPNAME="Moe-s9+-$(date '+%Y%m%d').zip"
-TC_DIR="$HOME/tc/clang-17.0.0"
+TC_DIR="$HOME/tc/clang-20.0.0"
 GCC_64_DIR="$HOME/tc/aarch64-linux-android-4.9"
 GCC_32_DIR="$HOME/tc/arm-linux-androideabi-4.9"
 AK3_DIR="$HOME/android/AnyKernel3"
