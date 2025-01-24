c91a4a4fd4752f149ad3bc7cc088a85b252e3f9b
shoiyadesu! <akariondev@gmail.com>
2025-01-24 15:44:01 -0300

    Update moe.sh

--- moe.sh
+++ moe.sh
@@ -3,12 +3,12 @@
 # Copyright (C) 2020-2021 Adithya R.
 
 SECONDS=0
-ZIPNAME="MoeKSU-ginkgo-$(date '+%Y%m%d').zip"
-TC_DIR="$HOME/tc/clang-20.0.0"
+ZIPNAME="MoeKSU-hanoip-$(date '+%Y%m%d').zip"
+TC_DIR="$HOME/tc/clang-19.0.0"
 GCC_64_DIR="$HOME/tc/aarch64-linux-android-15.0"
 GCC_32_DIR="$HOME/tc/arm-linux-androideabi-15.0"
 AK3_DIR="$HOME/android/AnyKernel3"
-DEFCONFIG="vendor/moe_defconfig"
+DEFCONFIG="vendor/hanoip_defconfig"
 
 export PATH="$TC_DIR/bin:$PATH"
 
@@ -17,7 +17,7 @@ export KBUILD_BUILD_HOST=Nyan
 
 if ! [ -d "${TC_DIR}" ]; then
     echo "Clang not found! Cloning to ${TC_DIR}..."
-    if ! git clone --depth=1 https://gitlab.com/moehacker/clang-r498229b ${TC_DIR}; then
+    if ! git clone --depth=1 https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r530567.git ${TC_DIR}; then
         echo "Cloning failed! Aborting..."
         exit 1
     fi
@@ -82,7 +82,7 @@ if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && \
     echo -e "\nKernel compiled successfully! Zipping up...\n"
     if [ -d "$AK3_DIR" ]; then
         cp -r $AK3_DIR AnyKernel3
-    elif ! git clone -q https://github.com/MoeKernel/AnyKernel3; then
+    elif ! git clone -q -b hanoip https://github.com/MoeKernel/AnyKernel3; then
         echo -e "\nAnyKernel3 repo not found locally and cloning failed! Aborting..."
         exit 1
     fi
@@ -90,7 +90,7 @@ if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && \
     cp out/arch/arm64/boot/dtbo.img AnyKernel3
     rm -f *zip
     cd AnyKernel3
-    git checkout master &> /dev/null
+    git checkout hanoip &> /dev/null
     zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
     cd ..
     rm -rf AnyKernel3
